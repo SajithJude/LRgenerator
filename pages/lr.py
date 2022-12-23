1627e95d7aae1113328aa9a8a0410a2d53fea172
SajithJude <judesajith.aj@gmail.com>
2022-12-24 00:42:43 +0530

    sdddsd

--- pages/lr.py
+++ pages/lr.py
@@ -4,11 +4,23 @@ import streamlit as st
 import openai
 import os
 
+@st.cache
+def get_input_list():
+  return []
+
+
 st.title("Input title of the reasearch papers")
+num =   st.slider("how many questions do you want to generate ?",min_value=5,max_value=30)
+
+for i in len(num):
+    user_input = st.text_input("enter the url of the research paper")
+    input_list = get_input_list()
+    input_list.append(user_input)
+    
+st.write('The list is now :', input_list)
 # st.text("#adjust the slider  to fine tune the number of questions you want in the output")
 
 url= st.text_input("enter the url of the research paper")
-num =   st.slider("how many questions do you want to generate ?",min_value=6,max_value=30)
 
 if st.button("Analyze"):
 
