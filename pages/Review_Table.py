158ec3913346c54db71696af9d28e705afe27c7c
SajithJude <judesajith.aj@gmail.com>
2022-12-24 03:01:05 +0530

    dsd

--- pages/Review_Table.py
+++ pages/Review_Table.py
@@ -4,7 +4,9 @@ import streamlit as st
 # df = pd.DataFrame(columns=['url', 'algo','contrib','LR','ref','tech'])
 # st.text("#adjust the slider  to fine tune the number of questions you want in the output")
 if "df_result" not in st.session_state:
-    st.session_state['df_result'] = "no data"
-    st.write("No Data, Generate some information in Fetch Information page to add data")
+    try:
+        st.session_state['df_result'] = "no data"
+    except:
+        st.write("No Data, Generate some information in Fetch Information page to add data")
 
 st.table(st.session_state['df_result'])
