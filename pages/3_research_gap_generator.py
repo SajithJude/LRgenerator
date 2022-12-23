13a78a807e9dce9f8723c01784c9228acd5d5666
SajithJude <judesajith.aj@gmail.com>
2022-12-24 03:17:34 +0530

    dsd

--- pages/3_research_gap_generator.py
+++ pages/3_research_gap_generator.py
@@ -10,24 +10,25 @@ openai.api_key =  os.getenv("APIKEY")
 try:
     links = st.session_state['df_result']['url']
     st.write(links)
+    if st.button("generate Research Gap from Urls in table"):
+    # st.session_state['max_tries']  = st.session_state['max_tries']  -1
+    # if st.session_state['max_tries'] >0:
+        gap = openai.Completion.create(
+        model="text-davinci-002",
+        prompt="Find a research gap in the following publications :" + links,
+        temperature=0.56,
+        max_tokens=3600,
+        top_p=1,
+        frequency_penalty=0.35,
+        presence_penalty=0,
+        # stop=["\n"]
+        )
+        st.subheader("Research Gap")
+        st.write(gap.choices[0].text)
+    x = str(gap.choices[0].text)
     # for link in links:
 
 except:
     st.write("No Data, Generate some information in Fetch Information page to add data")
 
-if st.button("generate Research Gap from Urls in table"):
-    # st.session_state['max_tries']  = st.session_state['max_tries']  -1
-    # if st.session_state['max_tries'] >0:
-    gap = openai.Completion.create(
-    model="text-davinci-002",
-    prompt="Find a research gap in the following publications :" + links,
-    temperature=0.56,
-    max_tokens=3600,
-    top_p=1,
-    frequency_penalty=0.35,
-    presence_penalty=0,
-    # stop=["\n"]
-    )
-    st.subheader("Research Gap")
-    st.write(gap.choices[0].text)
-    x = str(gap.choices[0].text)
+
