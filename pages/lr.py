d3ae0b325124b4bbbf77ba2970397e1dcebac7ee
SajithJude <judesajith.aj@gmail.com>
2022-12-24 01:21:57 +0530

    dsd

--- pages/lr.py
+++ pages/lr.py
@@ -9,12 +9,17 @@ data = {'url': [''], 'algo':['']}
 df = pd.DataFrame(data)
 # st.text("#adjust the slider  to fine tune the number of questions you want in the output")
 
-url1= st.text_input("enter the url of the research paper")
+
+def add(url,x):
+    df_addrow = pd.DataFrame([[url1, x]], columns=['url','algo'])
+    df = df.append(df_addrow, ignore_index=True)
+    return df
 
 
 
 if st.button("Analyze"):
 
+    url1= st.text_input("enter the url of the research paper")
 
     openai.api_key =  os.getenv("APIKEY")
 
@@ -30,14 +35,16 @@ if st.button("Analyze"):
     # stop=["\n"]
     )
     st.write(response.choices[0].text)
+    x = str(response.choices[0].text)
     # st.write(response)
 
-    df_addrow = pd.DataFrame([[url1, str(response.choices[0].text)]], columns=['url','algo'])
-    df = df.append(df_addrow, ignore_index=True)
+    if st.button("add"):
+        c = add(url,x)
+
 
 
     # df['url'].append(url1)
     # df['algo'] = str(response.choices[0].text)
 
     st.write(df)
-    
+
