c16aea3706d69b76fc5cfad1c071ccc5d96c6a36
SajithJude <judesajith.aj@gmail.com>
2022-12-24 01:36:57 +0530

    dsd

--- pages/lr.py
+++ pages/lr.py
@@ -21,34 +21,33 @@ def add(url,x):
 
 
 
-if st.button("Analyze"):
 
-    url1= st.text_input("enter the url of the research paper")
+url1= st.text_input("enter the url of the research paper")
 
-    openai.api_key =  os.getenv("APIKEY")
+openai.api_key =  os.getenv("APIKEY")
 
 
-    response = openai.Completion.create(
-    model="text-davinci-002",
-    prompt="what are the algorithms used in this publication :" + url1  +" .",
-    temperature=0.56,
-    max_tokens=3600,
-    top_p=1,
-    frequency_penalty=0.35,
-    presence_penalty=0,
-    # stop=["\n"]
-    )
-    st.write(response.choices[0].text)
-    x = str(response.choices[0].text)
-    # st.write(response)
+response = openai.Completion.create(
+model="text-davinci-002",
+prompt="what are the algorithms used in this publication :" + url1  +" .",
+temperature=0.56,
+max_tokens=3600,
+top_p=1,
+frequency_penalty=0.35,
+presence_penalty=0,
+# stop=["\n"]
+)
+st.write(response.choices[0].text)
+x = str(response.choices[0].text)
+# st.write(response)
 
-    if st.button("add"):
-        c = add(url1,x)
+if st.button("add"):
+    c = add(url1,x)
 
 
 
     # df['url'].append(url1)
     # df['algo'] = str(response.choices[0].text)
 
-st.write(st.session_state.state)
+    st.write(c)
 
