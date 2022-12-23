a71eea069788edda1bba9e21b4f215215e54338a
SajithJude <judesajith.aj@gmail.com>
2022-12-24 02:38:06 +0530

    dsd

--- pages/Review_Table.py
+++ pages/Review_Table.py
@@ -4,6 +4,6 @@ import streamlit as st
 # df = pd.DataFrame(columns=['url', 'algo','contrib','LR','ref','tech'])
 # st.text("#adjust the slider  to fine tune the number of questions you want in the output")
 if "df_result" not in st.session_state:
-    st.session_state['df_result'] = df
+    st.session_state['df_result'] = "no data"
 
 st.table(st.session_state['df_result'])
