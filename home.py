c08c25898dd50ac629ab52983e77507b65ab5cdd
SajithJude <judesajith.aj@gmail.com>
2022-12-24 06:44:58 +0530

    dsd

--- home.py
+++ home.py
@@ -1,20 +1,45 @@
 import os
 import openai
+
+
+
+
 import streamlit as st
 
-
-
-
-
-def main():
-
-    st.title("LR generator")
-    # num =   st.slider("how many questions do you want to generate ?",min_value=6,max_value=30)
-
+st.markdown("# Jude Sajith")
+st.markdown("## Pythonista")
+st.markdown("""
     
+This tool extracts information from the given URL for novel contributions, 
+technologies used, algorithm/methodology used and also generates literature reviews.
+ It allows users to quickly evaluate the research presented
+  and identify possible research gaps without the need to manually source and read through the content. 
+  It uses advanced algorithms to quickly scan and analyze the given URL and generate clear and concise reports. 
+  The reports are tailored to deliver easy-to-understand summaries of the research,
+   allowing users to quickly gain insights. It is useful for researchers, academics, 
+   and business professionals who require quick and reliable evaluation of research content.
+""")
 
+with st.container():
+    image_col, text_col = st.columns((1,2))
+    with image_col:
+        st.image("https://cdn-images-1.medium.com/max/906/1*dVSDol9pouoO9IX_E_-35Q.png")
 
-if __name__ == "__main__":
-    openai.api_key =  os.getenv("APIKEY")
+    with text_col:
+        st.subheader("A Multi-page Interactive Dashboard with Streamlit and Plotly")
+        st.write("""Beautiful interactive multipage dashboards are made easy with Streamlit
+            """)
+        st.markdown("[Read more...](https://towardsdatascience.com/a-multi-page-interactive-dashboard-with-streamlit-and-plotly-c3182443871a)")
 
-    main()
+with st.container():
+    image_col, text_col = st.columns((1,2))
+    with image_col:
+        st.image("https://cdn-images-1.medium.com/max/906/1*hjhCIWGgLzOznTFwDyeIeA.png")
+
+    with text_col:
+        st.subheader("Rational UI Design with Streamlit")
+        st.write("""
+            From one point of view Streamlit is a retrograde step in web development because 
+            it lets you mix up the logic of your app with the way it is presented. But from 
+            another it is very much simplifying web design.""")
+        st.markdown("[Read more...](https://towardsdatascience.com/rational-ui-design-with-streamlit-61619f7a6ea4)")
