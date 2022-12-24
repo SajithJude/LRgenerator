9b807a1b444c6c1d93c67cd07c5b26dee16402f5
SajithJude <judesajith.aj@gmail.com>
2022-12-24 06:48:39 +0530

    dsds

--- home.py
+++ home.py
@@ -22,26 +22,26 @@ technologies used, algorithm/methodology used and also generates literature revi
    and business professionals who require quick and reliable evaluation of research content.
 """)
 
-with st.container():
-    image_col, text_col = st.columns((1,2))
-    with image_col:
-        st.image("https://cdn-images-1.medium.com/max/906/1*dVSDol9pouoO9IX_E_-35Q.png")
+# with st.container():
+#     image_col, text_col = st.columns((1,2))
+#     with image_col:
+#         st.image("https://cdn-images-1.medium.com/max/906/1*dVSDol9pouoO9IX_E_-35Q.png")
 
-    with text_col:
-        st.subheader("A Multi-page Interactive Dashboard with Streamlit and Plotly")
-        st.write("""Beautiful interactive multipage dashboards are made easy with Streamlit
-            """)
-        st.markdown("[Read more...](https://towardsdatascience.com/a-multi-page-interactive-dashboard-with-streamlit-and-plotly-c3182443871a)")
+#     with text_col:
+#         st.subheader("A Multi-page Interactive Dashboard with Streamlit and Plotly")
+#         st.write("""Beautiful interactive multipage dashboards are made easy with Streamlit
+#             """)
+#         st.markdown("[Read more...](https://towardsdatascience.com/a-multi-page-interactive-dashboard-with-streamlit-and-plotly-c3182443871a)")
 
-with st.container():
-    image_col, text_col = st.columns((1,2))
-    with image_col:
-        st.image("https://cdn-images-1.medium.com/max/906/1*hjhCIWGgLzOznTFwDyeIeA.png")
+# with st.container():
+#     image_col, text_col = st.columns((1,2))
+#     with image_col:
+#         st.image("https://cdn-images-1.medium.com/max/906/1*hjhCIWGgLzOznTFwDyeIeA.png")
 
-    with text_col:
-        st.subheader("Rational UI Design with Streamlit")
-        st.write("""
-            From one point of view Streamlit is a retrograde step in web development because 
-            it lets you mix up the logic of your app with the way it is presented. But from 
-            another it is very much simplifying web design.""")
-        st.markdown("[Read more...](https://towardsdatascience.com/rational-ui-design-with-streamlit-61619f7a6ea4)")
+#     with text_col:
+#         st.subheader("Rational UI Design with Streamlit")
+#         st.write("""
+#             From one point of view Streamlit is a retrograde step in web development because 
+#             it lets you mix up the logic of your app with the way it is presented. But from 
+#             another it is very much simplifying web design.""")
+#         st.markdown("[Read more...](https://towardsdatascience.com/rational-ui-design-with-streamlit-61619f7a6ea4)")
